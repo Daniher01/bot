7bb75649ae81751f3e10b8a7154a71773b9ece8b
Daniher01 <daniher02@gmail.com>
2021-12-17 18:26:00 -0300

    se agregan mas estrategias

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from indicadores import *
 
 def cruce_hma(df, periodo1, periodo2):
-    precio = float(df['close'][-1:])
+    precio_actual = float(df['close'][-1:])
+    vela_anterior = float(df['close'][-2:-1])
+    dos_velas_antes = float(df['close'][-3:-2])
     hma50 = HMA(df, periodo1)
     hma80 = HMA(df, periodo2)
-    if hma80 > hma50 and precio > hma80:
+    if hma80 > hma50 and dos_velas_antes > hma80 and dos_velas_antes > vela_anterior and vela_anterior > precio_actual:
         print('se esta cumpliendo la estrategia')
         return True
     else:
