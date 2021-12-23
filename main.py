6977f017b93c9265e69418aba49da1a619a39062
Daniher01 <daniher02@gmail.com>
2021-12-23 18:47:05 -0300

    se agrega la clase bot en el main

--- main.py
+++ main.py
@@ -1,21 +1,33 @@
+import pandas as pd
 from binance.exceptions import BinanceAPIException, BinanceOrderException
 import conexion
-import estrategias
+import funciones
 
 
 class CriptoBot():
-    def __init__(self, cripto, time, data_df):
+    def __init__(self, cripto, time, fecha):
         self.cliente = conexion.cliente
         self.cripto = cripto
         self.time = time #temporalidad
-        self.data_df = data_df
+        self.fecha = fecha
+        self.data_df = funciones.datos_ticker(self.cripto, self.time, self.fecha)
+        """Valores por defecto"""
+        self.HMA_L = 80,
+        self.HMA_C = 50
 
     def log(self):
         """Resgistro de actividad del bot"""
         pass
 
+    def parser(self, data):
+        data_parser = pd.concat([self.data_df, data])
+        print(data_parser)
+        return data_parser
+
     def last_data(self):
         """actualizar datos necesarios para la toma de decisiones"""
+        data_update = self.data_df.iloc[[-1]]
+        self.data_df = self.parser(data_update)
         pass
 
     def estrategia(self):
@@ -27,3 +39,8 @@ class CriptoBot():
         pass
 
 
+#bot = CriptoBot('BTCUSDT','1d','marzo 2021')
+#bot.last_data()
+
+d = funciones.datos_ticker('BTCUSDT', '1d', '81 days ago')
+print(d)
