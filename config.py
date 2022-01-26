77133f920e3a3819d7b5119f79d9f6ea4cba9a03
Daniher01 <daniher02@gmail.com>
2022-01-26 13:37:52 -0300

    se borran los tokens

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 """
 conectarse con api de binance
 """
-API_KEY = "YMyzl8mN57FpmZ8px2KQQnEdwoSdUNnxSHeFouPShrGr4IAbKyXYDgxbmj0Y3wLP"
-API_SECRET = "aAaEbQfUTv3KscN9dH0tVllImhuGiyChmiTdd0ET9rmX2lQOrQARH3e3awWApkIt"
+API_KEY = ""
+API_SECRET = ""
 
 """
 conenctarse a la api de telegram
 """
-TOKEN = "5297947010:AAEPpS7wyUH7Q_mO3RUEju1jMp1lXK5bRsk"
-CHAT_ID = "929430484"
+TOKEN = ""
+CHAT_ID = ""
