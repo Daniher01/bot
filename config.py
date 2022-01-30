92b549d3b8596daf4fc13644e20ddc919287979a
Daniher01 <daniher02@gmail.com>
2022-01-29 21:32:27 -0300

    se arregla metodo run

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 """
 conectarse con api de binance
 """
-API_KEY = " "
-API_SECRET = " "
+API_KEY = "YMyzl8mN57FpmZ8px2KQQnEdwoSdUNnxSHeFouPShrGr4IAbKyXYDgxbmj0Y3wLP"
+API_SECRET = "aAaEbQfUTv3KscN9dH0tVllImhuGiyChmiTdd0ET9rmX2lQOrQARH3e3awWApkIt"
 
 """
 conenctarse a la api de telegram
 """
-TOKEN = " "
-CHAT_ID = " "
+TOKEN = "5297947010:AAEPpS7wyUH7Q_mO3RUEju1jMp1lXK5bRsk"
+CHAT_ID = "929430484"
