a6d2832f65c2cc33213cc948171cdb0c85795e10
Daniel Hernandez <72770636+Daniher01@users.noreply.github.com>
2022-08-09 16:05:19 -0400

    se quitan las claves de acceso
    por seguridad se quitan las claves de acceso de binance y telegram

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
 """
 conectarse con api de binance
 """
-API_KEY = "YMyzl8mN57FpmZ8px2KQQnEdwoSdUNnxSHeFouPShrGr4IAbKyXYDgxbmj0Y3wLP"
-API_SECRET = "aAaEbQfUTv3KscN9dH0tVllImhuGiyChmiTdd0ET9rmX2lQOrQARH3e3awWApkIt"
+API_KEY = ""
+API_SECRET = ""
 
 """
 conenctarse a la api de telegram
 """
-TOKEN = "5104192503:AAE-6TtuswXnlWa97gd2GxwBE2WCCekrcOQ"
-CHAT_ID = "929430484"
+TOKEN = ""
+CHAT_ID = ""
 
